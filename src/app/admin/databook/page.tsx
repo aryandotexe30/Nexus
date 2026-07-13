4a3d1fd95fd8cb04f85b3d085b5aced033c42b89
Aryan <aryan@example.com>
2026-07-13 14:34:28 +0530

    fix: remove extra div tag in databook modal

--- src/app/admin/databook/page.tsx
+++ src/app/admin/databook/page.tsx
@@ -378,7 +378,6 @@ export default function DatabookPage() {
                     </>
                   );
                 })()}
-              </div>    
               </div>
             </div>
           </div>
